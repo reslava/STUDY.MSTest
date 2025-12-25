597fdc5132fb81670249fd3d76db8ce17186f51e
reslava <rafeslava@hotmail.com>
2025-12-25 07:51:44 +0100

    Add project files.

---
+++ PrimeService.Tests/Test1.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Prime.Services;
+
+namespace Prime.UnitTests.Services
+{
+    [TestClass]
+    public class PrimeService_IsPrimeShould
+    {
+        private readonly PrimeService _primeService;
+
+        public PrimeService_IsPrimeShould ()
+        {
+            _primeService = new PrimeService ();
+        }
+
+        [TestMethod]
+        public void IsPrime_InputIs1_ReturnFalse ()
+        {
+            bool result = _primeService.IsPrime (1);
+
+            Assert.IsFalse (result, "1 shouldn't be prime");
+        }
+    }
+}
