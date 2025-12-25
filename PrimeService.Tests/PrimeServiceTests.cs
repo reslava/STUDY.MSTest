e59cf8f8087bf2691de2ef19a67de2afb8e709b1
reslava <rafeslava@hotmail.com>
2025-12-25 08:23:16 +0100

    Prime function and multiple tests

--- PrimeService.Tests/PrimeServiceTests.cs
+++ PrimeService.Tests/PrimeServiceTests.cs
@@ -5,20 +5,41 @@ namespace Prime.UnitTests.Services
 {
     [TestClass]
     public class PrimeService_IsPrimeShould
-    {
-        private readonly PrimeService _primeService;
-
-        public PrimeService_IsPrimeShould ()
-        {
-            _primeService = new PrimeService ();
-        }
-
+    {        
         [TestMethod]
         public void IsPrime_InputIs1_ReturnFalse ()
         {
-            bool result = _primeService.IsPrime (1);
+            bool result = PrimeService.IsPrime (1);
 
             Assert.IsFalse (result, "1 shouldn't be prime");
+        }
+
+        [TestMethod]
+        [DataRow (-1)]
+        [DataRow (0)]
+        [DataRow (1)]
+        public void IsPrime_ValuesLessThan2_ReturnFalse (int value)
+        {
+            var result = PrimeService.IsPrime (value);
+
+            Assert.IsFalse (result, $"{value} should not be prime");
+        }
+
+        [TestMethod]
+        [DataRow (-1, false)]
+        [DataRow (0, false)]
+        [DataRow (1, false)]
+        [DataRow (7, true)]
+        [DataRow (13, true)]
+        [DataRow (15, false)]
+        [DataRow (19, true)]
+        [DataRow (22, false)]
+        [DataRow (23, true)]
+        public void IsPrime_ValuesLessThan2_ReturnFalse (int value, bool output)
+        {
+            var result = PrimeService.IsPrime (value);
+
+            Assert.AreEqual (result, output, $"{value} {output}");                
         }
     }
 }
--- PrimeService/PrimeService.cs
+++ PrimeService/PrimeService.cs
@@ -4,13 +4,16 @@ namespace Prime.Services
 {
     public class PrimeService
     {
-        public bool IsPrime (int candidate)
+        public static bool IsPrime (int candidate)
         {
-            if (candidate == 1)
+            if (candidate <= 1) return false;
+            if (candidate == 2) return true;
+            if (candidate % 2 == 0) return false;
+            for (int i = 3; i * i <= candidate; i += 2)
             {
-                return false;
+                if (candidate % i == 0) return false;
             }
-            throw new NotImplementedException ("Please create a test first.");
+            return true;
         }
     }
 }
