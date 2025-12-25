907455512815e92c63d754cb561723c7ef961d99
reslava <rafeslava@hotmail.com>
2025-12-25 08:27:01 +0100

    better message for error in test

--- PrimeService.Tests/PrimeServiceTests.cs
+++ PrimeService.Tests/PrimeServiceTests.cs
@@ -39,7 +39,7 @@ namespace Prime.UnitTests.Services
         {
             var result = PrimeService.IsPrime (value);
 
-            Assert.AreEqual (result, output, $"{value} {output}");                
+            Assert.AreEqual (result, output, $"{value} number should be {output} prime.");                
         }
     }
 }
